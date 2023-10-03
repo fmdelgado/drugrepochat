0deabca21f691aa3f42df58fe5837cfc793219c4
lisaSpindler <lischen.spindler@icloud.com>
2023-10-03 10:33:44 +0200

    added login + sign up with DB, added chat saving in DB

--- app/db_hack.py
+++ app/db_hack.py
@@ -3,19 +3,75 @@ import time
 import streamlit as st
 import openai
 import sys
+
 sys.path.append('/Users/fernando/Documents/Research/drugrepochat/app')
 from db_chat import user_message, bot_message, check_for_openai_key
 from my_pdf_lib import load_index_from_db, get_index_for_pdf, store_index_in_db
 import json
 import os
-import requests
-from PIL import Image
-from io import BytesIO
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from langchain.prompts import PromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
 import markdown
+import sqlite3
+import hashlib
+
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+
+
+def create_usertable():
+    c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT, key TEXT)')
+
+
+def delete_chat(user):
+    c.execute('DELETE FROM chattable WHERE username =?', [user])
+    conn.commit()
+
+
+def create_chattable():
+    c.execute('CREATE TABLE IF NOT EXISTS chattable(rowid INTEGER PRIMARY KEY,username TEXT, message TEXT, role TEXT)')
+
+def add_chatdata(username, message, role):
+    c.execute('INSERT INTO chattable(username,message, role) VALUES (?,?, ?)', (username, message, role))
+    conn.commit()
+
+def get_chatdata(username):
+    c.execute('SELECT * FROM chattable WHERE username =?', [username])
+    data = c.fetchall()
+    return data
+
+
+def add_userdata(username, password, key):
+    c.execute('INSERT INTO userstable(username,password, key) VALUES (?,?,?)', (username, password, key))
+    conn.commit()
+
+def update_key(key, user):
+    c.execute('UPDATE userstable SET key =? WHERE username =?', (key, user))
+    conn.commit()
+
+
+def check_if_user_already_exists(username):
+    c.execute('SELECT * FROM userstable WHERE username =?', [username])
+    data = c.fetchall()
+    return len(data) == 0
+
+
+def login_user(username, password):
+    c.execute('SELECT * FROM userstable WHERE username =? AND password = ?', (username, password))
+    data = c.fetchall()
+    return data
+
+
+def make_hashes(password):
+    return hashlib.sha256(str.encode(password)).hexdigest()
+
+
+def check_hashes(password, hashed_text):
+    if make_hashes(password) == hashed_text:
+        return hashed_text
+    return False
+
 
 def get_json(filename):
     with open(filename, 'r') as f:
@@ -45,11 +101,11 @@ def get_openai_models():
 
 
 def chat_page():
-    #api_key = check_for_openai_key()
-    #openai.api_key = api_key
+    # api_key = check_for_openai_key()
+    # openai.api_key = api_key
 
-    #available_models = get_openai_models()
-    #selected_model = st.selectbox("Please select a model", options=available_models)
+    # available_models = get_openai_models()
+    # selected_model = st.selectbox("Please select a model", options=available_models)
     st.title("Academate")
     st.header("Questions and  Answering Chatbot")
     st.write(
@@ -115,7 +171,7 @@ def chat_page():
 
         response = []
         result = ""
-        #if openai.api_key:
+        # if openai.api_key:
         if False:
             for chunk in openai.ChatCompletion.create(
                     model=selected_model, messages=prompt, temperature=0, stream=True
@@ -141,21 +197,22 @@ def chat_page():
 
 
 def chat_page2():
-    st.session_state.key = st.text_input(
-        "Please, type in your OpenAI API key to continue", type="password", help="at least 10 characters required"
-    )
-    if len(st.session_state.key) > 10:
-        openai.api_key = st.session_state.key
-    else:
-        st.warning("At least 10 characters are required!")
-        st.stop()
+    if not st.session_state.key:
+        st.session_state.key = st.text_input(
+            "Please, type in your OpenAI API key to continue", type="password", help="at least 10 characters required"
+        )
+        if len(st.session_state.key) > 10:
+            openai.api_key = st.session_state.key
+        else:
+            st.warning("At least 10 characters are required!")
+            st.stop()
 
     try:
         available_models = get_openai_models()
     except:
         st.error("Your provided OpenAI API key is not valid.")
         available_models = []
-        #st.stop()
+        # st.stop()
     selected_model = st.selectbox("Please select a model", options=available_models)
     st.title("Academate")
     st.header("Questions and  Answering Chatbot")
@@ -180,9 +237,17 @@ def chat_page2():
 
     index = load_index_from_db(index_name)
 
-    if "messages" not in st.session_state.keys() or len(st.session_state.messages)==0:
-        st.session_state.messages = [{"role": "assistant", "content": "Hi there, how can I help?"}]
+    if "messages" not in st.session_state.keys() or len(st.session_state.messages) == 0:
+        message = {"role": "assistant", "content": "Hi there, how can I help?"}
+        st.session_state.messages = [message]
+        if "user" in st.session_state.keys():
+            add_chatdata(st.session_state.user, message["content"], message["role"])
 
+    if "user" in st.session_state.keys():
+        messages = get_chatdata(st.session_state.user)
+        st.session_state.messages = []
+        for message in messages:
+            st.session_state.messages.append({"role": message[3], "content": message[2]})
     for message in st.session_state.messages:
         if message["role"] == "user":
             user_message(message["content"])
@@ -190,17 +255,20 @@ def chat_page2():
             bot_message(message["content"], bot_name="Academate")
 
     if prompt := st.chat_input():
-        st.session_state.messages.append({"role": "user", "content": prompt})
+        message = {"role": "user", "content": prompt}
+        st.session_state.messages.append(message)
+        if "user" in st.session_state.keys():
+            add_chatdata(st.session_state.user, message["content"], message["role"])
 
     if prompt:
         # rest of the function...
-        #WHY?
-        #docs = index.similarity_search(st.session_state.dummy_text)
-        #doc = docs[0].page_content
+        # WHY?
+        # docs = index.similarity_search(st.session_state.dummy_text)
+        # doc = docs[0].page_content
 
-        #prompt_template = 'The given information is: {document_data}'
-        #prompt_template = prompt_template.format(document_data=doc)
-        #prompt[0] = {"role": "system", "content": prompt_template}
+        # prompt_template = 'The given information is: {document_data}'
+        # prompt_template = prompt_template.format(document_data=doc)
+        # prompt[0] = {"role": "system", "content": prompt_template}
 
         with st.container():
             user_message(prompt)
@@ -208,7 +276,7 @@ def chat_page2():
 
         response = []
         result = ""
-        if openai.api_key and len(available_models)>0:
+        if openai.api_key and len(available_models) > 0:
             for chunk in openai.ChatCompletion.create(
                     model=selected_model, messages=prompt, temperature=0, stream=True
             ):
@@ -218,20 +286,27 @@ def chat_page2():
                     result = "".join(response).strip()
 
                     botmsg.update(result)
-
-            st.session_state.messages.append({"role": "assistant", "content": result})
+            message = {"role": "assistant", "content": result}
+            st.session_state.messages.append(message)
+            if "user" in st.session_state.keys():
+                add_chatdata(st.session_state.user, message["content"], message["role"])
 
         else:
             failure_message = """
             Hi there. You haven't provided me with an OpenAI API key that I can use. 
             Please provide a key, so we can start chatting!
             """
-            st.session_state.messages.append({"role": "assistant", "content": failure_message})
+            message = {"role": "assistant", "content": failure_message}
+            st.session_state.messages.append(message)
             botmsg.update(failure_message)
+            if "user" in st.session_state.keys():
+                add_chatdata(st.session_state.user, message["content"], message["role"])
         # Add clear chat button
-    if len(st.session_state.messages)!=0 and st.button("Clear Chat"):
+    if len(st.session_state.messages) != 0 and st.button("Clear Chat"):
         st.session_state.messages = []
+        delete_chat(st.session_state.user)
         st.experimental_rerun()
+
 
 def config_page():
     openai_api_key = check_for_openai_key()
@@ -292,7 +367,7 @@ def config_page():
                 indices.remove(select_index)
                 store_data_as_json("index-list.json", indices)
             else:
-                #cannot be seen because of the rerun -> time sleep for one second
+                # cannot be seen because of the rerun -> time sleep for one second
                 st.warning("Database is protected and cannot be deleted.")
                 time.sleep(1)
             st.experimental_rerun()
@@ -316,7 +391,7 @@ def qanda_page():
 
     available_models = get_openai_models()
     selected_model = st.selectbox("Please select a model", options=available_models, index=0)
-    chaintype = st.selectbox("Please select chain type", options=['stuff', "map_reduce","refine"], index=0)
+    chaintype = st.selectbox("Please select chain type", options=['stuff', "map_reduce", "refine"], index=0)
     default_k = 4
     selected_k = st.slider("k", min_value=1, max_value=50, value=default_k, step=1)
     default_fetch_k = 20
@@ -384,16 +459,15 @@ def qanda_page():
                 input_variables=["user_prompt"])
 
             # Create the chain to answer questions
-            qa_chain = RetrievalQA.from_chain_type(llm=ChatOpenAI(temperature=0.0, model=selected_model, openai_api_key=openai.api_key),
-                                                   chain_type=chaintype,
-                                                   retriever=index.as_retriever(search_type="mmr",
-                                                                                search_kwargs={'fetch_k': selected_fetch_k,
-                                                                                               'k': selected_k}),
-                                                   return_source_documents=True,
-                                                   verbose=True)
+            qa_chain = RetrievalQA.from_chain_type(
+                llm=ChatOpenAI(temperature=0.0, model=selected_model, openai_api_key=openai.api_key),
+                chain_type=chaintype,
+                retriever=index.as_retriever(search_type="mmr",
+                                             search_kwargs={'fetch_k': selected_fetch_k,
+                                                            'k': selected_k}),
+                return_source_documents=True,
+                verbose=True)
             llm_response = qa_chain(formatted_prompt.format_prompt(user_prompt=st.session_state.dummy_text).to_string())
-
-
 
             # text = f"{llm_response['result']}\nSources:\n{process_llm_response(llm_response, doc_content=showdocs)}"
             # Convert Markdown to HTML
@@ -467,12 +541,69 @@ def about_page():
     """)
 
 
+def sign_up():
+    st.subheader("Create an Account")
+    st.session_state.user = st.text_input('Username')
+    st.session_state.password = st.text_input('Password', type='password')
+    st.session_state.key = st.text_input(
+        "Please, type in your OpenAI API key to continue", type="password", help="at least 10 characters required"
+    )
+    if len(st.session_state.key) > 10:
+        openai.api_key = st.session_state.key
+    else:
+        st.warning("At least 10 characters are required!")
+        st.stop()
+    if st.button('SignUp'):
+        create_usertable()
+        if check_if_user_already_exists(st.session_state.user):
+            add_userdata(st.session_state.user, make_hashes(st.session_state.password), st.session_state.key)
+            st.success("You have successfully created an account.")
+            st.session_state.logged_in = True
+        else:
+            st.error("The user already exists. Please create a new user or login.")
+            st.session_state.logged_in = False
+
+
+def login():
+    st.subheader("Login")
+    st.session_state.user = st.text_input('Username')
+    st.session_state.password = st.text_input('Password', type='password')
+    create_usertable()
+    create_chattable()
+    hashed_pswd = make_hashes(st.session_state.password)
+    result = login_user(st.session_state.user, check_hashes(st.session_state.password, hashed_pswd))
+    if result:
+        st.success("Logged In as {}".format(st.session_state.user))
+        st.session_state.key = result[0][2]
+        if st.checkbox("change provided OpenAI API key"):
+            st.session_state.key = st.text_input(
+                "Please, type in your OpenAI API key to continue", type="password",
+                help="at least 10 characters required"
+            )
+            if len(st.session_state.key) > 10:
+                openai.api_key = st.session_state.key
+                update_key(st.session_state.key, st.session_state.user)
+                st.success("Your OpenAI API key has been changed successfully!")
+            else:
+                st.warning("At least 10 characters are required!")
+                st.stop()
+    elif len(st.session_state.user) > 0 and len(st.session_state.password) > 0:
+        st.error("Incorrect login!")
+
+def logout():
+    st.session_state.user = None
+    st.session_state.password = None
+    st.session_state.key = None
+
 
 def main():
     page = st.sidebar.selectbox(
         "Choose a page",
-        ["Chatbot", "Configure knowledge base", "Q&A", "About"],
+        ["Login", "Sign up", "Chatbot", "Configure knowledge base", "Q&A", "About"],
     )
+    with st.sidebar:
+        if st.button("logout"):
+            logout()
 
     if page == "Chatbot":
         chat_page2()
@@ -480,10 +611,16 @@ def main():
         config_page()
     elif page == "Q&A":
         qanda_page()
-    elif page== "Visualize index":
+    elif page == "Visualize index":
         visualize_index_page()
     elif page == "About":
         about_page()
+    elif page == "Sign up":
+        sign_up()
+    elif page == "Login":
+        login()
+
+
 
 
 if __name__ == "__main__":
