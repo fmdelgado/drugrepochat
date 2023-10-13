1e776497be70d9f7e0ef1bd15276833050abea4c
lisaSpindler <lischen.spindler@icloud.com>
2023-10-13 13:22:25 +0200

    db update to mysql, mysql server has to be set up to use the application

--- app/db_management.py
+++ app/db_management.py
@@ -1,82 +1,104 @@
-import sqlite3
+import mysql.connector
 
-conn = sqlite3.connect('data.db', check_same_thread=False)
-c = conn.cursor()
+# database and user have to be created first of all
+db_name = "data"
+user_db = "chat_bot"
+pw_db = "Pysg1eS8pR3SpD?nv}RW"
 
-#create tables
+conn = mysql.connector.connect(host='localhost', user=user_db, password=pw_db, database=db_name)
+c = conn.cursor(buffered=True)
+
+
+# create tables
 def create_usertable():
-    c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT, key TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS userstable(user VARCHAR(100) PRIMARY KEY,password TEXT, apikey TEXT);')
+
 
 def create_knowledgebase():
-    c.execute('CREATE TABLE IF NOT EXISTS knowledgebase(username TEXT, name TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS knowledgebase(user TEXT, name TEXT);')
+
 
 def create_chattable():
-    c.execute('CREATE TABLE IF NOT EXISTS chattable(rowid INTEGER PRIMARY KEY,username TEXT, message TEXT, role TEXT)')
+    c.execute(
+        'CREATE TABLE IF NOT EXISTS chattable(rowid INTEGER auto_increment PRIMARY KEY,user TEXT, message TEXT, role TEXT);')
+
 
 def create_qandatable():
-    c.execute('CREATE TABLE IF NOT EXISTS qandatable(rowid INTEGER PRIMARY KEY,username TEXT, message TEXT, role TEXT)')
-
+    c.execute(
+        'CREATE TABLE IF NOT EXISTS qandatable(rowid INTEGER auto_increment PRIMARY KEY,user TEXT, message TEXT, role TEXT);')
 
 
 def delete_chat(user):
-    c.execute('DELETE FROM chattable WHERE username =?', [user])
+    c.execute('DELETE FROM chattable WHERE user = %s;', [user])
     conn.commit()
+
 
 def delete_knowledgebase(user, name):
-    c.execute('DELETE FROM knowledgebase WHERE username =? AND name=?', (user, name))
+    c.execute('DELETE FROM knowledgebase WHERE user = %s AND name= %s;', (user, name))
     conn.commit()
+
 
 def delete_qanda(user):
-    c.execute('DELETE FROM qandatable WHERE username =?', [user])
+    c.execute('DELETE FROM qandatable WHERE user = %s;', [user])
     conn.commit()
+
 
 def add_chatdata(username, message, role):
-    c.execute('INSERT INTO chattable(username,message, role) VALUES (?,?, ?)', (username, message, role))
+    c.execute('INSERT INTO chattable(user,message, role) VALUES (%s, %s, %s);', (username, message, role))
     conn.commit()
+
 
 def add_qandadata(username, message, role):
-    c.execute('INSERT INTO qandatable(username,message, role) VALUES (?,?, ?)', (username, message, role))
+    c.execute('INSERT INTO qandatable(user,message, role) VALUES (%s, %s, %s);', (username, message, role))
     conn.commit()
+
 
 def add_userdata(username, password, key):
-    c.execute('INSERT INTO userstable(username,password, key) VALUES (?,?,?)', (username, password, key))
+    c.execute('INSERT INTO userstable(user,password, apikey) VALUES (%s, %s, %s);', (username, password, key))
     conn.commit()
+
 
 def add_knowledgebase(username, name):
-    c.execute('INSERT INTO knowledgebase(username, name) VALUES (?,?)', (username, name))
+    c.execute('INSERT INTO knowledgebase(user, name) VALUES (%s , %s);', (username, name))
     conn.commit()
+
+
 def get_chatdata(username):
-    c.execute('SELECT * FROM chattable WHERE username =?', [username])
+    c.execute('SELECT * FROM chattable WHERE user = %s;', [username])
     data = c.fetchall()
     return data
+
 
 def get_knowledgebases_per_user(username):
-    c.execute('SELECT * FROM knowledgebase WHERE username =?', [username])
+    c.execute('SELECT * FROM knowledgebase WHERE user = %s;', [username])
     data = c.fetchall()
     return data
+
 
 def get_qandadata(username):
-    c.execute('SELECT * FROM qandatable WHERE username =?', [username])
+    c.execute('SELECT * FROM qandatable WHERE user = %s;', [username])
     data = c.fetchall()
     return data
+
 
 def get_user_data(username):
-    c.execute('SELECT * FROM userstable WHERE username =?', [username])
+    c.execute('SELECT * FROM userstable WHERE user = %s;', [username])
     data = c.fetchall()
     return data
 
+
 def update_key(key, user):
-    c.execute('UPDATE userstable SET key =? WHERE username =?', (key, user))
+    c.execute('UPDATE userstable SET apikey = %s WHERE user = %s;', (key, user))
     conn.commit()
 
 
 def check_if_user_already_exists(username):
-    c.execute('SELECT * FROM userstable WHERE username =?', [username])
+    c.execute('SELECT * FROM userstable WHERE user = %s;', [username])
     data = c.fetchall()
     return len(data) == 0
 
 
 def login_user(username, password):
-    c.execute('SELECT * FROM userstable WHERE username =? AND password = ?', (username, password))
+    c.execute('SELECT * FROM userstable WHERE user = %s AND password = %s ;', (username, password))
     data = c.fetchall()
     return data
